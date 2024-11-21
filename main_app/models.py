040ee696d022fda9b3ec9b6be1b13314764aefe4
Elder-one <aleksan672@gmail.com>
2024-11-22 01:38:04 +0300

    Still tryna fix img problem ...

--- main_app/models.py
+++ main_app/models.py
@@ -12,7 +12,7 @@ class Recipe(models.Model):
     author = models.ForeignKey(UserExtended, on_delete=models.CASCADE, related_name='recipies')
     title = models.CharField(max_length=150, null=False)
     category = models.ForeignKey(RecipeCategory, related_name='recipies', on_delete=models.CASCADE, null=True)
-    image = models.ImageField(upload_to="/home/Elder9one/Cootorials/images/", null=False)
+    image = models.ImageField(upload_to="images/", null=False)
     short_description = models.CharField(max_length=250, null=False)
     tutorial = models.TextField(null=False, default='')
     uploaded_on = models.DateTimeField(auto_now_add=True)
